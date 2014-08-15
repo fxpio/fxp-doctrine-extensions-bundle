aadfdea08174b822274998adcd73fd0783cb9724
François Pluchino <francois.pluchino@sonatra.com>
2014-08-15 15:25:47 +0200

    Split unique entity validator class

--- Validator/Constraints/UniqueEntityValidator.php
+++ Validator/Constraints/UniqueEntityValidator.php
@@ -14,7 +14,6 @@ namespace Sonatra\Bundle\DoctrineExtensionsBundle\Validator\Constraints;
 use Doctrine\Common\Persistence\ManagerRegistry;
 use Doctrine\Common\Persistence\Mapping\ClassMetadata;
 use Doctrine\Common\Persistence\ObjectManager;
-use Doctrine\ORM\EntityManager;
 use Symfony\Component\Validator\Constraint;
 use Symfony\Component\Validator\ConstraintValidator;
 use Sonatra\Bundle\DoctrineExtensionsBundle\Exception\UnexpectedTypeException;
@@ -55,7 +54,7 @@ class UniqueEntityValidator extends ConstraintValidator
     {
         /* @var UniqueEntity $constraint */
 
-        $em = $this->getObjectManager($entity, $constraint);
+        $em = Util::getObjectManager($this->registry, $entity, $constraint);
         $fields = (array) $constraint->fields;
         $criteria = $this->getCriteria($entity, $constraint, $em);
 
@@ -85,12 +84,12 @@ class UniqueEntityValidator extends ConstraintValidator
     private function getResult($entity, Constraint $constraint, array $criteria, ObjectManager $em)
     {
         /* @var UniqueEntity $constraint */
-        $filters = $this->findFilters($em, (array) $constraint->filters, $constraint->allFilters);
+        $filters = Util::findFilters($em, (array) $constraint->filters, $constraint->allFilters);
 
-        $this->actionFilter($em, 'disable', $filters);
+        Util::actionFilter($em, 'disable', $filters);
         $repository = $em->getRepository(get_class($entity));
         $result = $repository->{$constraint->repositoryMethod}($criteria);
-        $this->actionFilter($em, 'enable', $filters);
+        Util::actionFilter($em, 'enable', $filters);
 
         if (is_array($result)) {
             reset($result);
@@ -118,77 +117,6 @@ class UniqueEntityValidator extends ConstraintValidator
                 && $entity === ($result instanceof \Iterator
                     ? $result->current()
                     : current($result)));
-    }
-
-    /**
-     * Pre validate entity.
-     *
-     * @param object     $entity
-     * @param Constraint $constraint
-     *
-     * @return ObjectManager
-     *
-     * @throws UnexpectedTypeException
-     * @throws ConstraintDefinitionException
-     */
-    private function getObjectManager($entity, Constraint $constraint)
-    {
-        $this->validateConstraint($constraint);
-        /* @var UniqueEntity $constraint */
-
-        return $this->findObjectManager($entity, $constraint);
-    }
-
-    /**
-     * @param Constraint $constraint
-     *
-     * @throws UnexpectedTypeException
-     * @throws ConstraintDefinitionException
-     */
-    private function validateConstraint(Constraint $constraint)
-    {
-        if (!$constraint instanceof UniqueEntity) {
-            throw new UnexpectedTypeException($constraint, __NAMESPACE__.'\UniqueEntity');
-        }
-
-        if (!is_array($constraint->fields) && !is_string($constraint->fields)) {
-            throw new UnexpectedTypeException($constraint->fields, 'array');
-        }
-
-        if (null !== $constraint->errorPath && !is_string($constraint->errorPath)) {
-            throw new UnexpectedTypeException($constraint->errorPath, 'string or null');
-        }
-
-        if (0 === count((array) $constraint->fields)) {
-            throw new ConstraintDefinitionException('At least one field has to be specified.');
-        }
-    }
-
-    /**
-     * @param object       $entity
-     * @param UniqueEntity $constraint
-     *
-     * @return ObjectManager
-     *
-     * @throws ConstraintDefinitionException
-     */
-    private function findObjectManager($entity, UniqueEntity $constraint)
-    {
-        if ($constraint->em) {
-            $em = $this->registry->getManager($constraint->em);
-
-            if (!$em) {
-                throw new ConstraintDefinitionException(sprintf('Object manager "%s" does not exist.', $constraint->em));
-            }
-        } else {
-            $em = $this->registry->getManagerForClass(get_class($entity));
-
-            if (!$em) {
-                throw new ConstraintDefinitionException(sprintf('Unable to find the object manager associated with an entity of class "%s".', get_class($entity)));
-            }
-        }
-
-        return $em;
     }
 
     /**
@@ -292,65 +220,6 @@ class UniqueEntityValidator extends ConstraintValidator
                 );
             }
             $criteria[$fieldName] = array_pop($relatedId);
-        }
-    }
-
-    /**
-     * Get the list of SQL Filter name must to be disabled.
-     *
-     * @param ObjectManager $om      The ObjectManager instance
-     * @param array         $filters The list of SQL Filter
-     * @param bool          $all     Force all SQL Filter
-     *
-     * @return array
-     */
-    private function findFilters(ObjectManager $om, array $filters, $all = false)
-    {
-        if (!$om instanceof EntityManager || (empty($filters) && !$all)) {
-            return array();
-        }
-
-        $all = ($all && !empty($filters)) ? false : $all;
-        $enabledFilters = $om->getFilters()->getEnabledFilters();
-
-        return $this->doFindFilters($filters, $enabledFilters, $all);
-    }
-
-    /**
-     * Do find filters.
-     *
-     * @param array $filters
-     * @param array $enabledFilters
-     * @param bool  $all
-     *
-     * @return array
-     */
-    private function doFindFilters(array $filters, array $enabledFilters, $all)
-    {
-        $reactivateFilters = array();
-
-        foreach ($enabledFilters as $name => $filter) {
-            if (in_array($name, $filters) || $all) {
-                $reactivateFilters[] = $name;
-            }
-        }
-
-        return $reactivateFilters;
-    }
-
-    /**
-     * Disable/Enable the SQL Filters.
-     *
-     * @param ObjectManager $om      The ObjectManager instance
-     * @param string        $action  Value : disable|enable
-     * @param array         $filters The list of SQL Filter
-     */
-    private function actionFilter(ObjectManager $om, $action, array $filters)
-    {
-        if ($om instanceof EntityManager) {
-            foreach ($filters as $name) {
-                $om->getFilters()->$action($name);
-            }
         }
     }
 }
---
+++ Validator/Constraints/Util.php
@@ -0,0 +1,157 @@
+<?php
+
+/*
+ * This file is part of the Sonatra package.
+ *
+ * (c) François Pluchino <francois.pluchino@sonatra.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sonatra\Bundle\DoctrineExtensionsBundle\Validator\Constraints;
+
+use Doctrine\Common\Persistence\ManagerRegistry;
+use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\ORM\EntityManager;
+use Sonatra\Bundle\DoctrineExtensionsBundle\Exception\ConstraintDefinitionException;
+use Sonatra\Bundle\DoctrineExtensionsBundle\Exception\UnexpectedTypeException;
+use Symfony\Component\Validator\Constraint;
+
+/**
+ * @author François Pluchino <francois.pluchino@sonatra.com>
+ */
+class Util
+{
+    /**
+     * Get the list of SQL Filter name must to be disabled.
+     *
+     * @param ObjectManager $om      The ObjectManager instance
+     * @param array         $filters The list of SQL Filter
+     * @param bool          $all     Force all SQL Filter
+     *
+     * @return array
+     */
+    public static function findFilters(ObjectManager $om, array $filters, $all = false)
+    {
+        if (!$om instanceof EntityManager || (empty($filters) && !$all)) {
+            return array();
+        }
+
+        $all = ($all && !empty($filters)) ? false : $all;
+        $enabledFilters = $om->getFilters()->getEnabledFilters();
+
+        return static::doFindFilters($filters, $enabledFilters, $all);
+    }
+
+    /**
+     * Do find filters.
+     *
+     * @param array $filters
+     * @param array $enabledFilters
+     * @param bool  $all
+     *
+     * @return array
+     */
+    public static function doFindFilters(array $filters, array $enabledFilters, $all)
+    {
+        $reactivateFilters = array();
+
+        foreach ($enabledFilters as $name => $filter) {
+            if (in_array($name, $filters) || $all) {
+                $reactivateFilters[] = $name;
+            }
+        }
+
+        return $reactivateFilters;
+    }
+
+    /**
+     * Disable/Enable the SQL Filters.
+     *
+     * @param ObjectManager $om      The ObjectManager instance
+     * @param string        $action  Value : disable|enable
+     * @param array         $filters The list of SQL Filter
+     */
+    public static function actionFilter(ObjectManager $om, $action, array $filters)
+    {
+        if ($om instanceof EntityManager) {
+            foreach ($filters as $name) {
+                $om->getFilters()->$action($name);
+            }
+        }
+    }
+
+    /**
+     * Pre validate entity.
+     *
+     * @param ManagerRegistry $registry
+     * @param object          $entity
+     * @param Constraint      $constraint
+     *
+     * @return ObjectManager
+     *
+     * @throws UnexpectedTypeException
+     * @throws ConstraintDefinitionException
+     */
+    public static function getObjectManager(ManagerRegistry $registry, $entity, Constraint $constraint)
+    {
+        static::validateConstraint($constraint);
+        /* @var UniqueEntity $constraint */
+
+        return static::findObjectManager($registry, $entity, $constraint);
+    }
+
+    /**
+     * @param Constraint $constraint
+     *
+     * @throws UnexpectedTypeException
+     * @throws ConstraintDefinitionException
+     */
+    private static function validateConstraint(Constraint $constraint)
+    {
+        if (!$constraint instanceof UniqueEntity) {
+            throw new UnexpectedTypeException($constraint, __NAMESPACE__.'\UniqueEntity');
+        }
+
+        if (!is_array($constraint->fields) && !is_string($constraint->fields)) {
+            throw new UnexpectedTypeException($constraint->fields, 'array');
+        }
+
+        if (null !== $constraint->errorPath && !is_string($constraint->errorPath)) {
+            throw new UnexpectedTypeException($constraint->errorPath, 'string or null');
+        }
+
+        if (0 === count((array) $constraint->fields)) {
+            throw new ConstraintDefinitionException('At least one field has to be specified.');
+        }
+    }
+
+    /**
+     * @param ManagerRegistry $registry
+     * @param object          $entity
+     * @param UniqueEntity    $constraint
+     *
+     * @return ObjectManager
+     *
+     * @throws ConstraintDefinitionException
+     */
+    private static function findObjectManager(ManagerRegistry $registry, $entity, UniqueEntity $constraint)
+    {
+        if ($constraint->em) {
+            $em = $registry->getManager($constraint->em);
+
+            if (!$em) {
+                throw new ConstraintDefinitionException(sprintf('Object manager "%s" does not exist.', $constraint->em));
+            }
+        } else {
+            $em = $registry->getManagerForClass(get_class($entity));
+
+            if (!$em) {
+                throw new ConstraintDefinitionException(sprintf('Unable to find the object manager associated with an entity of class "%s".', get_class($entity)));
+            }
+        }
+
+        return $em;
+    }
+}
